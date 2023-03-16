77424508a4168a58a7235de385aa6a8cdb5b0152
wanghy <119291641+icey-yu@users.noreply.github.com>
2023-03-16 21:37:12 +0800

    fix: 登录

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import (
 	_ "bot-Alice/internal/core"
 	_ "bot-Alice/internal/packed"
 	"bot-Alice/internal/service"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
 )
 
 func main() {
+	ctx := gctx.New()
 	//cmd.Main.Run(gctx.New())
-	service.Login()
+	err := service.Login().Login()
+	if err != nil {
+		g.Log().Error(ctx, "登录失败", err)
+	}
 
 	// 停了
 	select {}
